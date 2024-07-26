9f2ee6bf71bd39bb19282a09411088dccefdb656
Camilo Londoño <camilo89@hotmail.es>
2024-07-25 22:32:31 -0500

    Add: upload packages ROS2 Beginners Level1

---
+++ my_robot_controller/my_robot_controller/pose_subscriber.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+
+class PoseSubscriberNode(Node):
+    def __init__(self):
+        super().__init__("Pose_Subscriber")
+        self.pose_subscriber = self.create_subscription(Pose, "/turtle1/pose", self.pose_callback, 10)
+
+    def pose_callback(self, msg: Pose):
+        self.get_logger().info("msg: " + str(msg))
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PoseSubscriberNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
